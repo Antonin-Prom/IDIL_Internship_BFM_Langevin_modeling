ab8ba99b9bfc7d38c0c868379398694e7713ab61
Antonin Prom <133413251+Antonin-Prom@users.noreply.github.com>
2024-02-19 16:59:48 +0100

    histogram+dwell

--- langevin_simu/hist_angles.py
+++ langevin_simu/hist_angles.py
@@ -2,13 +2,22 @@ from scipy.integrate import quad
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-sys.path.append('/home/antonin/Documents/GitHub/IDIL_Internship_BFM_Langevin_modeling/langevin_simu/')
+#sys.path.append('/home/antonin/Documents/GitHub/IDIL_Internship_BFM_Langevin_modeling/langevin_simu/')
 from Class_particle_in_potential import *
 
-A = 2
-part = DiffusionSimulation(dt = 1e-6,torque = 20)
-traj = part.proceed_traj1(10000000,A)
-plt.hist(traj,bins = 1000,density=True)
+
+
+traj_matrix = []
+def generate_mean_traj(N,L,A,torque):
+    part = DiffusionSimulation(dt = 1e-5,torque = 100)
+    for i in range(N):
+        traj = part.proceed_traj1(L ,A)
+        traj_matrix.append(traj)
+    mean_traj = np.mean(traj_matrix,axis=0)
+    return mean_traj
+
+mean_traj = generate_mean_traj(1,1000000,3,100)
+plt.hist(mean_traj,bins = 2600,density=True)
 plt.xlabel('angle [rad]')
 plt.ylabel('density')
 plt.show()
