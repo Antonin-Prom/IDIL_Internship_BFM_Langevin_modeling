1bbf01ebde02d41072c98d77621b898e691af5b4
SlitheringBait <antonin.prom1@gmail.com>
2024-03-12 10:13:45 +0100

    theory_change

--- langevin_simu/Theory_defaveri.py
+++ langevin_simu/Theory_defaveri.py
@@ -117,11 +117,51 @@ def plot_integrand(t,A):
     plt.ylabel('u.a')
     plt.title(f'integrand at time {t:}s, amplitude = 6kT')
 
-plot_integral_theory()
+
+def quadratic(a,b,x):
+    return a*x*x + b*x
 
 
+def msd_integral_with_torque(time,A,torque):
+    part = DiffusionSimulation(torque=torque)
+    D_LJ = part.lifson_jackson_noforce(A)
+    msd = []
+    for t in time :
+        def integrand_with_torque(y,A,torque):
+            return y*y*np.exp(-torque*y*np.sqrt(t))*np.exp(-part.tilted_periodic_potential(A, y*np.sqrt(t)))*np.exp(-y*y/(4*D_LJ))/np.sqrt(4*np.pi*part.rotational_einstein_diff)
+        lower_limit = -2
+        upper_limit = 2
+        result, error = quad(integrand_with_torque, lower_limit, upper_limit,epsabs=1e-8, epsrel=1e-6)
+        msd.append(t*result)
+    return np.array(msd)
+
+def plot_msd_integral_with_torque():
+    torque = 5
+    aa = (2*np.pi/26)**2
+    K = 0.1648/(aa)
+    time_dta = np.logspace(-4,7,100)*K
+    A = 1
+    msd = msd_integral_with_torque(time_dta,A,torque)
+    plt.loglog(time_dta,msd/aa,label = 'V_0/kT = 2')
+    plt.xlabel('Dt/a²')
+    plt.ylabel('<x²>/a²')
+    
+    params, covariance = curve_fit(linear_D, time_dta, msd)
+    D = params[0]
+    plt.plot(time_dta,linear_D(time_dta, params[0], params[1]),label = f'fit, {D:.5f}')
+    
+def integrand_with_torque(y,A,torque,t):
+    part = DiffusionSimulation(torque=torque)
+    D_LJ = part.lifson_jackson_noforce(A)
+    return y*y*np.exp(-torque*y*np.sqrt(t))*np.exp(-part.tilted_periodic_potential(A, y*np.sqrt(t)))*np.exp(-y*y/(4*D_LJ))/np.sqrt(4*np.pi*part.rotational_einstein_diff)
+
+def plot_integrand_torque(t,A,torque):
+    y = np.linspace(-5,5,10000)
+    plt.plot(y,integrand_with_torque(y,A,torque,t))
+    plt.xlabel('y')
+    plt.ylabel('u.a')
+    plt.title(f'Integrand at time {t:}s, amplitude = {A}kT,torque = {torque}kT')
+plot_integrand_torque(10,1,1)
 plt.legend()
-
-
 print(msd)
 plt.show()
