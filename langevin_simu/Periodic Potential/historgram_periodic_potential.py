6105f52bd06bb4afc6f391055cd4669590beac69
Antonin Prom <133413251+Antonin-Prom@users.noreply.github.com>
2024-04-30 17:24:50 +0200

    msd_defaveri+hist+LJ

--- langevin_simu/Periodic Potential/historgram_periodic_potential.py
+++ langevin_simu/Periodic Potential/historgram_periodic_potential.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 #sys.path.append('/home/antonin/Documents/GitHub/IDIL_Internship_BFM_Langevin_modeling/langevin_simu/')
-from Class_particle_v2_drift_diffusion import *
+from Class_particle_v2_periodic_potential import *
 from scipy.stats import norm
 from scipy.optimize import curve_fit
 
@@ -21,7 +21,7 @@ def generate_mean_traj(N,L,A,torque):
 
 
 def get_trajs(amplitude,dt):
-    trajs = np.load(f'trajectories_10000points_amplitude_0kT_dt_1e-05_torque_20kT.npy')                  
+    trajs = np.load(f'trajectories_10000000points_amplitude_{amplitude}kT_dt_{dt}_torque_0kT.npy')                  
     return trajs
 
 def hist_at_t(amplitude,dt,t):
@@ -30,80 +30,47 @@ def hist_at_t(amplitude,dt,t):
     return matrix_t
 
 
-def plot_hist(amplitude,dt,i):
+def total_hist(amplitude,dt):
+    """
+    Combine the whole histogram of every traj in a single one : meaningless
+
+    Parameters
+    ----------
+    amplitude : TYPE
+        DESCRIPTION.
+    dt : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
     trajs = get_trajs(amplitude,dt)
-    traj = trajs[i]
-    plt.hist(traj,bins = 2600,density=True)
+    num_bins = 10000
+    accumulated_hist = np.zeros(num_bins)
+    for traj in trajs:
+        hist, _ = np.histogram(traj, bins=num_bins,range=(-np.pi, np.pi))
+        accumulated_hist += hist
+    
+    mean_hist = accumulated_hist*num_bins / (len(trajs)*len(trajs[0]))
+    bins_space = np.linspace(-np.pi,np.pi,num_bins)
+    plt.plot(bins_space,mean_hist)
     plt.xlabel('angle [rad]')
     plt.ylabel('density')
     plt.show()
-
-def normal_theory(mu,t,x):
-    part = DiffusionSimulation2()
-    D = part.rotational_einstein_diff
-    t = t*1e-5
-    p_theory = (1/np.sqrt(4*np.pi*D*t))*np.exp(- ((x - mu)**2)/(4*D*t))
-    return p_theory
-
-def normal_longtime_correction(mu,t,x):
-    part = DiffusionSimulation2()
-    D = part.rotational_einstein_diff
-    y = part.rotational_drag
-    t = t*1e-5
-    p_theory = np.sqrt(y/(2*np.pi*D))*np.exp(- (y*x**2)/(2*D))
-    return p_theory
-
-def normal_theory_full_correction(mu,t,x):
-    part = DiffusionSimulation2(dt=1e-5)
-    D = part.rotational_einstein_diff
-    y = part.rotational_drag*1000
-    t = t*1e-5
-    interm = y/(2*D*(1-np.exp(-2*y*t)))
-    p_theory = np.sqrt(y/(2*np.pi*D*(1-np.exp(-2*y*t))))*np.exp(-interm*x**2)
-    return p_theory
-
-                             
-t = 8000
-matrix_at_t = hist_at_t(0,1e-5,t)
-
-plt.hist(matrix_at_t,bins = 150,density=True)
-(mu, sigma) = norm.fit(matrix_at_t)
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, 100)
-p = norm.pdf(x, mu, sigma)
-plt.plot(x, p, 'k', linewidth=2, label='Fitted Normal Distribution')    
     
-plt.plot(x,normal_theory(mu,t,x), linestyle='--', color='red', label='Theoretical Normal Distribution')
+def hist_at_t(t):
+    trajs = np.load('trajectories_1000000,nb_traj_1000points_amplitude_6kT,frequency_10_dt_0.001_torque_0kT.npy')
+    matrix_t = [trajs[i][t]  for i in range(len(trajs))]
+    return matrix_t
 
-plt.title(f'Histogram of positions at t={t*1e-5:.3f}s over 10000 realisations')
-plt.xlabel('angle [rad]')
-plt.ylabel('density')
-plt.legend()
-plt.show()
-
-
-
-"""
-fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-
-times = [0, 100, 1000, 10000]  # Adjust time values as needed
-for i, t in enumerate(times):
-    matrix_at_t = hist_at_t(0, 1e-5, t)
-    ax = axs[i//2, i%2]  # Get the current axis
-    ax.hist(matrix_at_t, bins=100, density=True)
-    (mu, sigma) = norm.fit(matrix_at_t)
-    xmin, xmax = ax.get_xlim()
-    x = np.linspace(xmin, xmax, 1000)
-    p = norm.pdf(x, mu, sigma)
-    ax.plot(x, p, 'k', linewidth=2, label='Fitted Normal Distribution')
-    #ax.plot(x, normal_theory(mu, t, x), linestyle='--', color='red', label='Theoretical Normal Distribution')
-    ax.plot(x, normal_theory_full_correction(mu, t, x), linestyle='--', color='green', label='Corrected Normal Distribution')
-    ax.plot(x, normal_longtime_correction(mu, t, x), linestyle='--', color='orange', label='normal_longtime_correction Distribution')
-    ax.set_title(f'Histogram at t={t*1e-5:.3f}s over 10000 realisations')
-    ax.set_xlabel('angle [rad]')
-    ax.set_ylabel('density')
-    ax.legend()
-
-plt.tight_layout()
-plt.show()
-"""
+def plot_hist(t):
+    plt.hist(hist_at_t(t),bins = 200,density=True)
+    plt.title(f'Histogram of positions at t={t}ms over 1000 realisations')
+    plt.xlabel('angle [rad]')
+    plt.ylabel('density')
+    plt.show()
+    
+# A faire: 10000 realisations à 2kT, trouver comment fixer la densité à 1
+    
---
+++ langevin_simu/Periodic Potential/msd_loglog_defaveri.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 10:56:43 2024
+
+@author: CBS
+"""
+from Class_particle_v2_periodic_potential import *
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+
+
+        
+def qth_moment(q,t,D_eff):
+    t *= (0.1648)/(2*np.pi/26)**2
+    return((2**q)/(np.sqrt(np.pi)))*(D_eff*t)**(q/2)*scipy.special.gamma((1+q)/2)
+    
+def second_moment(t,D_eff):
+    return(4/(np.sqrt(np.pi)))*(D_eff*t)*scipy.special.gamma(3/2)
+
+def generate_theory_msd(absciss,D_fit):
+    theoretical_msd = []
+    for t in absciss:
+        msd_theo = qth_moment(2,t,D_fit)
+        theoretical_msd.append(msd_theo)
+    return theoretical_msd
+
+def asdefaveri_msd(A,dt):
+    p = DiffusionSimulation2(dt=dt)
+    a = 2*np.pi/p.frequency
+    D = p.rotational_einstein_diff           
+    D_eff = p.lifson_jackson_noforce(A)
+    
+    def defaveri_stationary(A):
+        X = np.linspace(0,a,1000)
+        Y = boltz_exp(A,X)
+        Z = np.mean(Y)
+        return (np.mean((X**2)*Y)/Z)   
+     
+    def boltz_exp(A,x):
+        return np.exp(-p.tilted_periodic_potential(A, x))
+        
+    def linear_D(t,D,shift):
+        return (2*D*t + shift)/(a*a)
+    
+    t, msd = np.load(f't,msd,dt={dt},{A}kT.npy')
+    absciss = 2*D*t
+    norm_msd = msd/(a*a)
+    
+    min_absciss_value = 1  # Adjust this value as needed
+    
+    window_indices = np.where(absciss > min_absciss_value)[0]
+    absciss_window = absciss[window_indices]
+    norm_msd_window = norm_msd[window_indices]
+    
+    popt, pcov = scipy.optimize.curve_fit(linear_D, absciss_window, norm_msd_window,)
+    D_fit, shift = popt[0], popt[1]
+    print('D_fit = ',D_fit*2*D)
+    print('D_eff = ',D_eff)
+    stationary_state = defaveri_stationary(A) 
+    return absciss,norm_msd,D_fit,D_eff,shift,stationary_state,a
+
+def plot_msd_asdefvari(A,dt):
+    absciss,norm_msd,D_fit,D_eff,shift,stationary_state,a = asdefaveri_msd(A,dt)
+    
+    def linear_D(t,D,shift):
+        return (2*D*t + shift)/(a*a)
+    
+    plt.loglog(absciss,norm_msd,label = f'A = {A} kT')
+    if A == 6:
+        plt.loglog(absciss,stationary_state*np.ones(len(absciss)),linestyle ='--')
+    #plt.loglog(absciss[int(len(absciss)/2):],linear_D(absciss,D_eff,shift)[int(len(absciss)/2):],linestyle ='--',color='green')
+    frac = 1.5
+    #plt.loglog(absciss[int(len(absciss)/frac):],generate_theory_msd(absciss,D_eff)[int(len(absciss)/frac):],color='red')
+    #D_eff/(2*0.1648)
+    plt.loglog(absciss[int(len(absciss)/frac):],linear_D(absciss,D_fit,shift)[int(len(absciss)/frac):],linestyle ='--',color='black') #[int(len(absciss)/4):]
+
+for A in range(0,7,2):
+    plot_msd_asdefvari(A,0.001)
+plt.xlabel('Dt/L²')
+plt.ylabel(r'$\theta$/L²')
+plt.legend()
+plt.show()
