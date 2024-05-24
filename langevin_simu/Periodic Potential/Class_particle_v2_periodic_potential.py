358537ff8ca8ab3ebf53429b51713f9ec88f8160
Antonin Prom <133413251+Antonin-Prom@users.noreply.github.com>
2024-05-24 17:30:06 +0200

    histo+trajs v2

--- langevin_simu/Periodic Potential/Class_particle_v2_periodic_potential.py
+++ langevin_simu/Periodic Potential/Class_particle_v2_periodic_potential.py
@@ -36,7 +36,10 @@ def _make_trace(x, npts, dUdx, dt, gamma, thermnoise):
         force = -dUdx[dUdx_idxi]
         # Langevin eq:
         x[i+1] = x[i] + force*dt/gamma + thermnoise[i]
+        x[i+1] = np.mod(x[i+1], 2*np.pi)
+        #if np.isnan(x[i]): print(dUdx_idxi, force)
     return x
+
 
 @njit(parallel=True)
 def single_msd(traj, total_lag_time):
@@ -105,13 +108,14 @@ class DiffusionSimulation2:
             plt.ylabel('pot. [KT]')
         return U
     
-    def main_traj_numba(self, N, A, U ):
+    def main_traj_numba(self, N, A, U, x0, ide ):
         """
         Langevin integration using numba. The potential U is an argument of type array.
         U = self.make_potential_sin(self.x_pot,ampl = A)
         """
         dUdx = np.diff(U)/(2*np.pi/len(U))
         x = np.zeros(N) 
+        x[0] = x0[ide]
         thermnoise = np.sqrt(2*self.D*self.dt)*np.random.randn(N)
         x = _make_trace(x, N, dUdx, self.dt, self.gamma, thermnoise)   
         return x
@@ -195,19 +199,46 @@ class DiffusionSimulation2:
         return np.array(msd)  
     
     
-    def run_parallel_numba(self, repetitions=None, n_jobs=5, npts = None, Amplitude = None, torque = 0):
+    def run_parallel_numba(self, repetitions=None, n_jobs=5, npts = int(1e5), x0 = None, Amplitude = None, torque = 0,iteration = 0, save = True, print_time = False):
         ''' parallel computations of generate_traj() 
             parallel_out is a list (of len repetitions) of arrays "x(t)" (each of len npts)
         '''
         # Parallel:
-        print(f'run_serial_parallel(): Parallel working...')
+        if print_time == True:
+            print(f'run_serial_parallel(): Parallel working...')
         t0 = time.time()
         sin_pot = self.make_potential_sin(ampl = Amplitude) 
-        parallel_out = Parallel(n_jobs=n_jobs)(delayed(self.main_traj_numba)(N = npts,A = Amplitude, U = sin_pot) for i in range(repetitions))
-        print(f'run_serial_parallel(): Parallel done in {time.time() - t0:.1f} s')
-        np.save(f'trajectories_{npts:.0f},nb_traj_{repetitions}points_amplitude_{Amplitude}kT,frequency_{self.frequency}_dt_{self.dt}_torque_{torque:.0f}kT',parallel_out)
+        parallel_out = Parallel(n_jobs=n_jobs)(delayed(self.main_traj_numba)(N = npts,A = Amplitude, U = sin_pot, x0 = x0, ide=i) for i in range(repetitions))
+        if print_time == True:
+            print(f'run_serial_parallel(): Parallel done in {time.time() - t0:.1f} s')
+        if save == True:
+            np.save(f'{iteration}ite_trajectories_{npts:.0f},nb_traj_{repetitions}points_amplitude_{Amplitude}kT,frequency_{self.frequency}_dt_{self.dt}_torque_{torque:.0f}kT',parallel_out)
         return parallel_out
     
+    def meta_parallel_numba(self, repetitions=None, n_jobs=5, npts = None, x0s = None, Amplitude = None, torque = 0, save = True): #x0 is an array of x0
+        '''
+        Because trajectory length >= 1e6points induces segmentation error, take the last trajectory point and inject it as the x0 of a new trajectory
+        '''
+        paquet = 1e8
+        t0 = time.time()
+        print('meta initialisation...')
+        iteration = int(npts/paquet)
+        if iteration > 1:
+            old_trajs = self.run_parallel_numba(repetitions=repetitions, n_jobs=n_jobs, npts = int(paquet), x0 = x0s, Amplitude = Amplitude, torque = torque)
+            for i in range(iteration-1):
+                #print(f'iteration n°{i} at {time.time() - t0:.1f} s ')
+                all_x0 = [all_x0 := traj[-1] for traj in old_trajs] #getting all the x0
+                trajs = self.run_parallel_numba(repetitions=repetitions, n_jobs=n_jobs, npts = int(paquet), x0 = all_x0, Amplitude = Amplitude,  torque = torque, iteration = i,save=False)
+                old_trajs = np.concatenate((old_trajs,trajs),axis=1)
+        else :
+            old_trajs = self.run_parallel_numba(repetitions=repetitions, n_jobs=n_jobs, npts = npts, x0 = x0s, Amplitude = Amplitude, torque = torque)
+        print(f'Done in {time.time() - t0:.1f} s ')
+        total_len = len(old_trajs[0])
+        if save == True:
+            np.save(f'final_trajectories_{total_len:.0f},nb_traj_{repetitions}points_amplitude_{Amplitude}kT,frequency_{self.frequency}_dt_{self.dt}_torque_{torque:.0f}kT',old_trajs)
+
+          
+            
     def run_parallel(self, repetitions=None, n_jobs=5, npts = None, Amplitude = 0, torque = 0):
         ''' parallel computations of generate_traj() 
             parallel_out is a list (of len repetitions) of arrays "x(t)" (each of len npts)
@@ -267,12 +298,13 @@ class DiffusionSimulation2:
         print(f'mean_msd_no_chunk(): Parallel done in {time.time() - t0:.1f} s')
         return msd_results
     
-    def numba_msd(self, traj, time_end=1/4, msd_nbpt = 2000):
+    def numba_msd(self, traj, time_end=1/4, msd_nbpt = 2000,print_time=False):
         t0 = time.time()
         max_lagtime = int(len(traj) * time_end)
         total_lag_time = np.unique([int(lag) for lag in np.floor(np.logspace(0, (np.log10(max_lagtime)), msd_nbpt))])
         msd_results = single_msd(traj, total_lag_time)
-        print(f'MSD_numba done in {time.time() - t0:.1f} s')
+        if print_time==True:
+            print(f'MSD_numba done in {time.time() - t0:.1f} s')
         
         return msd_results 
         
@@ -310,7 +342,7 @@ class DiffusionSimulation2:
         msd_matrix = []
         max_lagtime = int(len(trajs[0]) * time_end)
         for i in range(len(trajs[:nb_traj])):
-            msd_matrix.append(self.numba_msd(trajs[i], time_end=1/4, msd_nbpt = 2000,n_jobs=5))
+            msd_matrix.append(self.numba_msd(trajs[i], time_end=1/4, msd_nbpt = 2000))
         mean_msd = np.concatenate(([0],np.mean(msd_matrix, axis=0)))
         time_axis = np.concatenate(([0],np.unique((np.floor(np.logspace(0, (np.log10(max_lagtime)), msd_nbpt)))))) 
         print(f'mean_msd_no_chunk(): Parallel done in {time.time() - t0:.1f} s')
@@ -327,7 +359,7 @@ class DiffusionSimulation2:
         print(f'mean_msd_no_chunk(): Parallel done in {time.time() - t0:.1f} s')
         return time_axis,mean_msd
     
-    def full_auto_trajs_mean_msd(self,ampl_range = np.arange(8),nb_traj = 30, npts = None ):
+    def full_auto_trajs_mean_msd(self,ampl_range = np.arange(8),nb_traj = 30, npts = int(1e8) ):
         """
         Generate trajectories, calculate and mean MSD for each value of amplitude.
         Parameters
@@ -346,11 +378,12 @@ class DiffusionSimulation2:
         """
         trajectories_box = []
         mean_msd_box = []
+        x0s = np.zeros(npts)
         for A in ampl_range:
-            trajs = self.run_parallel_numba(n_jobs=1,repetitions = nb_traj, Amplitude = A, npts = npts)
-            trajectories_box.append()
-            time_axis,mean_msd = self.mean_msd_and_time_numba(trajs, n_jobs=1, time_end=1/4, msd_nbpt = 2000, nb_traj = nb_traj)
-        
+            trajs = self.meta_parallel_numba(self, repetitions=None, n_jobs=5, npts = None, x0s = None, Amplitude = A, torque = 0)
+            trajectories_box.append(trajs)
+            time_axis,mean_msd = self.mean_msd_and_time_numba(trajs, time_end=1/4, msd_nbpt = 2000, nb_traj = nb_traj)
+            mean_msd_box.append(mean_msd)
         
         
         """
@@ -374,12 +407,9 @@ class DiffusionSimulation2:
         lifson_jackson1 = self.D * (2 * np.pi / self.frequency)**2 / ((self.factor1(amplitude)) * (self.factor1(-amplitude)))
         return lifson_jackson1 
         
-
-def compare_ampli(ampli_min,ampli_max,dt):
-    total_time = int(1e8*1e-5)
-    for A in range(ampli_min,ampli_max):
-        p = DiffusionSimulation2(dt=dt)
-        p.run_parallel( repetitions=50, n_jobs=5, npts = nb_points, Amplitude = Ampli, torque = 0)
+    def lifson_jackson_force(self, amplitude):
+        lifson_jackson2 = (self.D * (2 * np.pi / self.frequency)**2 / ((self.factor1(amplitude)) * (self.factor1(-amplitude)))) * (np.sinh(self.torque*(2*np.pi / self.frequency)/2))/(self.torque*(2*np.pi / self.frequency)/2)
+        return lifson_jackson2
 
     
 def store_msd_given_Amplitude(Ampli):
